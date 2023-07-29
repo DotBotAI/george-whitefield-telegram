361c26676fc64f773a38c5cff05e59e81add31d0
Michael Rice <122009119+allmichael@users.noreply.github.com>
2023-07-29 06:20:10 -0500

    Update constants.py

--- bot/commands/constants.py
+++ bot/commands/constants.py
@@ -8,9 +8,17 @@ Built-in commands:
 {commands}{admin_commands}
 
 AI shortcuts:
-{shortcuts}
 
-[More features →](https://github.com/nalgeon/pokitoki#readme)
+!explain <Bible verse or range of Bible verses> - I will explain a Bible verse to you and what it means. I can also handle a range of Bible verses as well.
+!preach - I will preach to you about what is happening in your modern times, the importance of Saving America and how you can help save it.
+!expose <Write something about something happening within US politics> - Send me some text about something happening today and I will expose the corruption.
+!teach - I will teach you something about the Bible, Jesus or the Christian faith.
+
+[Learn more about George Whitefield AI →](https://whitefield.ai) 
+[Learn more about Cheetah Agency →](https://cheetahagency.com) 
+[Learn more about DotBot →](https://dotbot.ai) 
+[Learn more about ADAM AI →](https://helloadam.ai) 
+[Learn more about We Develop AI →](https://wedevelop.ai) 
 """
 
 PRIVACY_MESSAGE = (
